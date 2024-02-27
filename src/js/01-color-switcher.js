84d53e0c70c2da6bc7fe70d66b6013c283b905f2
d3monw1ngs <tezzielumsit87@gmail.com>
2024-02-27 21:51:24 +0800

    update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+let intervalId;
+
+document.querySelector('[data-start]').addEventListener('click', startColorChange);
+document.querySelector('[data-stop]').addEventListener('click', stopColorChange);
+
+function startColorChange() {
+    intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function stopColorChange() {
+    clearInterval(intervalId);
+}
+
+function changeBackgroundColor() {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+}
+
 
